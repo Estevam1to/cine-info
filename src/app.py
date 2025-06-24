0a78283b6a72756815835c057990c2f750c35a34
Luís Estevam <luisestevam33@gmail.com>
2025-06-24 08:10:53 -0300

    chore: remove reload option from uvicorn run command in app.py

--- src/app.py
+++ src/app.py
@@ -22,4 +22,4 @@ app.include_router(movie.router)
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("app:app", host="0.0.0.0", port=5000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=5000)
